442ff8b6ea76b0757e0fa4948c8f9774d60f343c
Jayram Powar <jayrampowar102@gmail.com>
2026-02-08 12:48:18 +0530

    same changes as first

--- backend/index.js
+++ backend/index.js
@@ -94,6 +94,10 @@ await connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+if(process.env.NODE_ENV !== "production"){
+    server.listen(PORT, () => {
     console.log("Server is running on PORT: "  + PORT);
 });
+}
+
+export default server;
